8b767313ca45cfe395d4b5f05533b79157c7d6cf
Aditya Kumar <neelaadityakumar@gmail.com>
2025-02-26 23:29:04 +0530

    update innerhtml

--- traffic-light/index.js
+++ traffic-light/index.js
@@ -6,44 +6,30 @@ class TrafficLight {
     this.layout = layout;
     this.timer = null;
 
-    this.containerEl = document.createElement("div");
-    this.containerEl.classList.add("traffic-light-container");
-    this.containerEl.setAttribute("aria-live", "polite");
+    this.rootEl.innerHTML = `<div class="traffic-light-container ${
+      this.layout === "vertical" ? "traffic-light-container--vertical" : ""
+    }" aria-live="polite"></div>`;
+    this.containerEl = this.rootEl.querySelector(".traffic-light-container");
 
-    if (this.layout === "vertical") {
-      this.containerEl.classList.add("traffic-light-container--vertical");
-    }
-
-    this.rootEl.appendChild(this.containerEl);
     this.runTrafficLight();
-
     window.addEventListener("beforeunload", () => this.clearTimer());
   }
 
-  createLight(color) {
-    const lightEl = document.createElement("div");
-    lightEl.classList.add("light");
-    lightEl.setAttribute("aria-hidden", true);
-    if (color) lightEl.style.backgroundColor = color;
-    return lightEl;
-  }
-
   updateLight() {
-    this.containerEl.innerHTML = "";
     this.containerEl.setAttribute(
       "aria-label",
       `Current light: ${this.currentColor}`
     );
 
-    Object.keys(this.config).forEach((color) => {
-      this.containerEl.append(
-        this.createLight(
+    this.containerEl.innerHTML = Object.keys(this.config)
+      .map((color) => {
+        return `<div class="light" style="background-color: ${
           color === this.currentColor
             ? this.config[color].backgroundColor
-            : undefined
-        )
-      );
-    });
+            : "#555"
+        };" aria-hidden="true"></div>`;
+      })
+      .join("");
   }
 
   transitionLight() {
@@ -64,18 +50,16 @@ class TrafficLight {
   }
 }
 
-// Traffic Light Configurations
 const config = {
   green: { backgroundColor: "green", duration: 3000, next: "yellow" },
   yellow: { backgroundColor: "yellow", duration: 500, next: "red" },
   red: { backgroundColor: "red", duration: 4000, next: "green" },
 };
 
-// Attach to each traffic light wrapper
 document.querySelectorAll(".traffic-light").forEach((wrapper, index) => {
   new TrafficLight(wrapper, {
     initialColor: "red",
     config,
-    layout: index === 0 ? "vertical" : "horizontal", // First one vertical, second horizontal
+    layout: index === 0 ? "vertical" : "horizontal",
   });
 });
