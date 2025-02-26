68eea14bfa9a6fc7e61c842ed9fb2f99a119163f
Aditya Kumar <neelaadityakumar@gmail.com>
2025-02-26 20:19:23 +0530

    update carousel

--- image-carousel/script.js
+++ image-carousel/script.js
@@ -1,21 +1,57 @@
-const carouselInner = document.querySelector(".carousel-inner");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-let currentIndex = 0;
+class Carousel {
+  constructor(carouselElement, options = {}) {
+    this.carousel = carouselElement;
+    this.carouselInner = this.carousel.querySelector(".carousel-inner");
+    this.items = this.carousel.querySelectorAll(".carousel-item");
+    this.prevBtn = this.carousel.querySelector(".carousel-control.left");
+    this.nextBtn = this.carousel.querySelector(".carousel-control.right");
+    this.currentIndex = 0;
+    this.totalItems = this.items.length;
+    this.autoPlay = options.autoPlay || false;
+    this.intervalTime = options.intervalTime || 3000;
+    this.timer = null;
 
-const updateCarousel = () => {
-  const width = carouselInner.clientWidth;
-  carouselInner.style.transform = `translateX(-${currentIndex * width}px)`;
-};
+    this.init();
+  }
 
-prevBtn.addEventListener("click", () => {
-  currentIndex = currentIndex > 0 ? currentIndex - 1 : 3;
-  updateCarousel();
+  init() {
+    this.prevBtn.addEventListener("click", () => this.prevSlide());
+    this.nextBtn.addEventListener("click", () => this.nextSlide());
+    window.addEventListener("resize", () => this.updateCarousel());
+    if (this.autoPlay) this.startAutoPlay();
+  }
+
+  updateCarousel() {
+    const width = this.carouselInner.clientWidth;
+    this.carouselInner.style.transform = `translateX(-${
+      this.currentIndex * width
+    }px)`;
+  }
+
+  prevSlide() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.totalItems) % this.totalItems;
+
+    this.updateCarousel();
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.totalItems;
+    this.updateCarousel();
+  }
+
+  startAutoPlay() {
+    this.timer = setInterval(() => this.nextSlide(), this.intervalTime);
+    this.carousel.addEventListener("mouseover", () =>
+      clearInterval(this.timer)
+    );
+    this.carousel.addEventListener("mouseleave", () => this.startAutoPlay());
+  }
+}
+
+document.querySelectorAll(".carousel").forEach((carouselEl, index) => {
+  new Carousel(carouselEl, {
+    autoPlay: index === 0, // First carousel autoplay true, second false
+    intervalTime: index === 0 ? 5000 : 4000,
+  });
 });
-
-nextBtn.addEventListener("click", () => {
-  currentIndex = currentIndex < 3 ? currentIndex + 1 : 0;
-  updateCarousel();
-});
-
-window.addEventListener("resize", updateCarousel);
