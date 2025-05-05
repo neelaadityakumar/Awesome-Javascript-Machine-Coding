b9025e9298996992c66e6c4df34c5892fad9eda5
Aditya Kumar <neelaadityakumar@gmail.com>
2025-05-05 22:23:16 +0530

    remove autoplay

--- carousel/script.js
+++ carousel/script.js
@@ -1,20 +1,14 @@
 class Carousel {
   constructor(container, options) {
     this.container = container;
-    this.autoPlay = options.autoPlay || false;
-    this.intervalTime = options.intervalTime || 3000;
     this.images = options.images || [];
     this.currentIndex = 0;
-    this.timer = null;
     this.createCarousel();
-
     this.carouselInner = this.carousel.querySelector(".carousel-inner");
     this.items = this.carousel.querySelectorAll(".carousel-item");
     this.prevBtn = this.carousel.querySelector(".carousel-control.left");
     this.nextBtn = this.carousel.querySelector(".carousel-control.right");
-
     this.totalItems = this.items.length;
-
     this.init();
   }
 
@@ -43,7 +37,6 @@ class Carousel {
     this.prevBtn.addEventListener("click", () => this.prevSlide());
     this.nextBtn.addEventListener("click", () => this.nextSlide());
     window.addEventListener("resize", () => this.updateCarousel());
-    if (this.autoPlay) this.startAutoPlay();
   }
 
   updateCarousel() {
@@ -62,22 +55,12 @@ class Carousel {
     this.currentIndex = (this.currentIndex + 1) % this.totalItems;
     this.updateCarousel();
   }
-
-  startAutoPlay() {
-    this.timer = setInterval(() => this.nextSlide(), this.intervalTime);
-    this.carousel.addEventListener("mouseover", () =>
-      clearInterval(this.timer)
-    );
-    this.carousel.addEventListener("mouseleave", () => this.startAutoPlay());
-  }
 }
 
 const carouselContainer = document.querySelector(".container");
 
 const carousels = [
   {
-    autoPlay: true,
-    intervalTime: 5000,
     images: [
       { src: "https://picsum.photos/536/354", alt: "Image 1" },
       { src: "https://picsum.photos/536/352", alt: "Image 2" },
@@ -85,8 +68,6 @@ const carousels = [
     ],
   },
   {
-    autoPlay: true,
-    intervalTime: 5000,
     images: [
       { src: "https://picsum.photos/536/354", alt: "Image 1" },
       { src: "https://picsum.photos/536/352", alt: "Image 2" },
